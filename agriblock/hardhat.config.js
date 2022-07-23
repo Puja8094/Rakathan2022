3f9a1824c9022de7e31aee53f2c5e306d4e32772
Programming Pirates <dharmendra.20208039@mnnit.ac.in>
2022-07-23 10:44:25 +0530

    done

--- agriblock/hardhat.config.js
+++ agriblock/hardhat.config.js
@@ -2,14 +2,14 @@ require("@nomiclabs/hardhat-waffle");
 const INFURA_URL = 'infura_url';
 const PRIVATE_KEY = 'private_key';
 const POCKET_ENDPOINT = 'pocket_endpoint';
-module.exports = {
-    solidity: "0.8.4",
-    networks: {
-       rinkeby: {
-           url: INFURA_URL,
-           accounts: [`0x${PRIVATE_KEY}`]
-       }
-    },
+ module.exports = {
+     solidity: "0.8.4",
+//     networks: {
+//        rinkeby: {
+//            url: INFURA_URL,
+//            accounts: [`0x${PRIVATE_KEY}`]
+//        }
+//     },
     paths: {
     artifacts: "./src/backend/artifacts",
     sources: "./src/backend/contracts",
